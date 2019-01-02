354512424dc7319580154daec36250da07da6ec6
aarslann <arsslanakin@gmail.com>
2019-01-02 14:25:27 +0300

    index

--- views/frontend/default/index.php
+++ views/frontend/default/index.php
@@ -4,7 +4,7 @@
     <div class="jumbotron">
         <h1>Kou Osl Yii2 App</h1>
 
-        <p class="lead">Örnek uygulamayı başarılı bir şekilde çalıştırdınız.</p>
+        <p class="lead">Örnek uygulamayı başarılı bir şekilde çalıştırdınız!!!</p>
 
         <p><a class="btn btn-lg btn-success" href="#">Modüller ve konfürgasyon!</a></p>
     </div>
